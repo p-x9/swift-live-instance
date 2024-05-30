d470871264bfa22cc84f4c511d5ea3fde8b378aa
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-05-30 16:12:54 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "LiveInstance",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LiveInstance",
-            targets: ["LiveInstance"]),
+            targets: ["LiveInstance"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LiveInstance"),
+            name: "LiveInstance"
+        ),
         .testTarget(
             name: "LiveInstanceTests",
-            dependencies: ["LiveInstance"]),
+            dependencies: ["LiveInstance"]
+        ),
     ]
 )
